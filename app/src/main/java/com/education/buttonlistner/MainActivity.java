18dc974d052273470ef0af21af458bad4fa7cd1a
VinayTShetty <vinaytshetty@gmail.com>
2023-11-11 09:02:31 +0530

    Button OnClick implementing View.OnClicListner

--- app/src/main/java/com/education/buttonlistner/MainActivity.java
+++ app/src/main/java/com/education/buttonlistner/MainActivity.java
@@ -2,11 +2,34 @@ package com.education.buttonlistner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intializeWidget();
+        addOnClickListner();
     }
+
+    private void intializeWidget(){
+        button=(Button) findViewById(R.id.button_bt);
+    }
+    private void addOnClickListner(){
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.button_bt){
+            Toast.makeText(this, "Button Click Demo", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
