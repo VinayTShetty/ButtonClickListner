0121b68ddf7a8cd4602b0ebc9c82aa3aab58c242
VinayTShetty <vinaytshetty@gmail.com>
2023-11-11 08:05:55 +0530

    IntializeWidgets Demostration

--- app/src/main/java/com/education/buttonlistner/MainActivity.java
+++ app/src/main/java/com/education/buttonlistner/MainActivity.java
@@ -2,11 +2,32 @@ package com.education.buttonlistner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private TextView textView;
+    private Button button;
+    private Switch switchButton;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intializeWidgets();
+    }
+
+    private void intializeWidgets(){
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+        switchButton = findViewById(R.id.switchButton);
+        imageView = findViewById(R.id.imageView);
     }
 }
