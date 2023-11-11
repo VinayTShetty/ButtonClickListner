4c253c81657cd3ae0b41f31cacf3e6ec602af4a3
VinayTShetty <vinaytshetty@gmail.com>
2023-11-11 09:16:02 +0530

    Button OnClick implementing OnClickListner using annonymous Inner class.

--- app/src/main/java/com/education/buttonlistner/MainActivity.java
+++ app/src/main/java/com/education/buttonlistner/MainActivity.java
@@ -2,11 +2,31 @@ package com.education.buttonlistner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intializeWidget();
+        buttonClickListner();
+    }
+
+    private void intializeWidget(){
+        button=(Button)findViewById(R.id.button_bt);
+    }
+
+    private void buttonClickListner(){
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Button OnClicListner", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
