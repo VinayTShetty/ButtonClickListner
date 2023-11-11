dc62864c671d651e916b8063c316901357c45911
VinayTShetty <vinaytshetty@gmail.com>
2023-11-11 08:41:47 +0530

    Button OnClick Using XML Layout.

--- app/src/main/java/com/education/buttonlistner/MainActivity.java
+++ app/src/main/java/com/education/buttonlistner/MainActivity.java
@@ -2,11 +2,19 @@ package com.education.buttonlistner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClick(View view){
+        Toast.makeText(this, "Button 1 clicked!", Toast.LENGTH_SHORT).show();
     }
 }
